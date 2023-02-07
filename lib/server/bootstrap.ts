b112d25ea9f7cd178c73cbf853e418febd3f0309
Matthew Manela <mmanela@users.noreply.github.com>
2023-02-07 14:18:05 -0500

    fix

--- lib/server/bootstrap.ts
+++ lib/server/bootstrap.ts
@@ -1,7 +1,6 @@
 require('ignore-styles');
 require('url-loader');
-require('file-loader');
-require('sass-loader');
+require('file-loader'); 
 require('@babel/register')({
     ignore: [/(node_modules)/],
     presets: ['@babel/preset-env', '@babel/preset-react'],
